907d4572959c9930eb7cde4990e87dfc8d39c859
Irsyad Ashari <muh.ashari@gojek.com>
2025-12-15 09:16:02 +0700

    Initial Commit

---
+++ Video Minifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Video Minifier
+//
+//  Created by Muh Irsyad Ashari on 12/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Video Minifier/Video_MinifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Video_MinifierApp.swift
+//  Video Minifier
+//
+//  Created by Muh Irsyad Ashari on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Video_MinifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
